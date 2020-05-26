8e5fa30aa2ed99dab66078624c31a82c8e610173
unknown <Paulo Brando@N-1S-DEV-16.info.local>
2020-05-26 09:17:39 -0300

    Projeto 01 Criado

---
+++ 01_CadastroAluno/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_CadastroAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

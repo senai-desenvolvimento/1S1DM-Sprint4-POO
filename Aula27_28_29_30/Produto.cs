4e8ee05532c798404d48a7f9f404f393310f6568
Paulo Brandão <paulobrandaoofficial@gmail.com>
2020-06-29 09:54:50 -0300

    Aula 27 finalizada

---
+++ Aula27_28_29_30/Produto.cs
@@ -0,0 +1,36 @@
+using System.IO;
+
+namespace Aula27_28_29_30
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        private const string PATH = "Database/produto.csv";
+
+        public Produto()
+        {         
+            // Cria o arquivo caso não exista
+            if(!File.Exists(PATH))
+            {
+                File.Create(PATH).Close();
+            }
+        }
+
+        public void Cadastrar(Produto prod)
+        {            
+            string[] linha = new string[] { PrepararLinha(prod) };
+            File.AppendAllLines(PATH, linha);
+        }
+
+        // 1;Sapato;34,90
+        private string PrepararLinha(Produto p)
+        {
+            return $"codigo={p.Codigo};nome={p.Nome};preco={p.Preco}";
+        }
+
+
+    }
+}
---
+++ Aula27_28_29_30/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula27_28_29_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto();
+            p1.Codigo = 1;
+            p1.Nome = "Gibson";
+            p1.Preco = 5500f;
+
+            p1.Cadastrar(p1);
+        }
+    }
+}
